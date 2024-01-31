aea56295649dff140b9200cb4a352f7bab03f440
MUHAMMAD QAYYUM BIN JAMAL 2204719E <2204719E@student.tp.edu.sg>
2024-01-31 16:03:21 +0800

    Add project files.

---
+++ HomeCarePharmacy_V3/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using HomeCarePharmacy_V3.Server.Models;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace HomeCarePharmacy_V3.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
