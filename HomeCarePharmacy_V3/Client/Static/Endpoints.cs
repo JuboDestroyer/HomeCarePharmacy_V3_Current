11aa0f107c90439806f38b84d321cf95ce87a547
EMIR EZZUDDIN BIN MOHAMAD 2204787B <2204787B@student.tp.edu.sg>
2024-02-01 13:44:41 +0800

    Updated with staff pages

---
+++ HomeCarePharmacy_V3/Client/Static/Endpoints.cs
@@ -0,0 +1,14 @@
+﻿namespace HomeCarePharmacy_Project.Client.Static
+{
+    public static class Endpoints
+    {
+        private static readonly string Prefix = "api";
+
+        public static readonly string ProductsEndpoint = $"{Prefix}/products";
+        public static readonly string ConsultationsEndpoint = $"{Prefix}/consultations";
+        public static readonly string OrdersEndpoint = $"{Prefix}/orders";
+        public static readonly string OrderItemsEndpoint = $"{Prefix}/orderitems";
+        public static readonly string CustomersEndpoint = $"{Prefix}/customers";
+        public static readonly string StaffsEndpoint = $"{Prefix}/staffs";
+    }
+}
