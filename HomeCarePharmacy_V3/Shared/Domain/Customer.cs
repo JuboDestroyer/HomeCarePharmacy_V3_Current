99e3159b3abda4b55d0a3da63c1c48cae873b5e4
EMIR EZZUDDIN BIN MOHAMAD 2204787B <2204787B@student.tp.edu.sg>
2024-02-04 14:12:30 +0800

    Made edit for most except order items. Continue on view and trying to add form validation.

--- HomeCarePharmacy_V3/Shared/Domain/Customer.cs
+++ HomeCarePharmacy_V3/Shared/Domain/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,13 @@ namespace HomeCarePharmacy_V3.Shared.Domain
     public class Customer
     {
         public int CustomerId { get; set; }
+        
         public string? Name { get; set; }
+       
         public string? Address { get; set; }
+       
         public int PhoneNumber { get; set; }
+       
         public string? Email { get; set; }
 
     }
